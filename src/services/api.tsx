e2623e3e38a1e24f415f156871fa714feb1691b5
fairoznidhi <fairoz1697@gmail.com>
2025-01-01 15:40:51 +0600

    tanstack query

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { UserProfileDataType } from "@/app/(user)/UserProfile/page";
 
-const BASE_URL=`${process.env.BACKEND_URL}`;
+const BASE_URL=`${process.env.NEXT_PUBLIC_PROXY_URL}`;
 const axiosInstance=axios.create({baseURL:BASE_URL});
+
+console.log(BASE_URL)
 
 export const getSingleEmployee=async(id:number)=>{
     return (await axiosInstance.get<UserProfileDataType>(`employee?employee_id=${id}`)).data;
