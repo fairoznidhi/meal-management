e8c9f33c979d44efee5c5e4200bc2bde2406f195
fairoznidhi <fairoz1697@gmail.com>
2025-02-07 17:17:09 +0600

    admin dashboard conflict resolved

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -143,7 +143,7 @@ export default function AuthLayout({
               <Image
                 src={vslogo}
                 alt="vlogo"
-                className="w-10 h-10 border rounded-full bg-white ms-3  "
+                className="w-10 h-10 border rounded-full bg-white ms-3 me-1 "
               ></Image>
               {/*<p className="text-white font-semibold mt-1 text-2xl font-serif">
                 VivaMeal
