c3f68a055aec2136c66241188f321254521066cc
Noshin Tamanna <noshintamanna108@gmail.com>
2025-04-17 19:19:35 +0600

    user data type modified

--- src/services/types.ts
+++ src/services/types.ts
@@ -30,6 +30,19 @@ export type UserProfileDataType = {
   
 };
 
+export type UserEditDataType={
+   employee_id?: number;
+  name?: string;
+  email?: string;
+  phone_number?: string;
+  dept_name?: string;
+  remarks?: string;
+  default_status?: boolean;
+  is_admin?: boolean;
+  photo?: string | File | Blob;
+  preference_food: number[];
+}
+
 
 export type Preference = {
   food_Id: number;
